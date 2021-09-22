73ceda46a99bd0c394fdbfcfbd93cdaa1da2d7ca
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2021-09-21 21:28:21 -0600

    Secso? llamado 2

--- SOKOBAN_E.D/main.cpp
+++ SOKOBAN_E.D/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char* args[])
 {
 	Menu* partida1;
-	partida1 = new Menu(960, 540, "SOKOBANxdxd -_- A DARLE");
+	partida1 = new Menu(960, 540, "SOKOBANxdxd -_- A DARLE duro a ");
 	
 	return 0;
 }
